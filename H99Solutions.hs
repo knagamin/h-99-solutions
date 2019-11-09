ce9044f34bfe74f17cc1e402034e6ae8573dc56c
Kiyo Nagamine <knagamin.hub@gmail.com>
2019-11-09 22:44:29 +0900

    change: added a solution for Problem3

--- H99Solutions.hs
+++ H99Solutions.hs
@@ -5,3 +5,10 @@ myLast = head . reverse
 
 myButLast :: [a] -> a
 myButLast = myLast . init
+
+elementAt :: [a] -> Int -> a
+elementAt (x:_) 1 = x
+elementAt [] _ = error "Index error"
+elementAt (_:xs) n
+  | n < 1      = error "Index error"
+  | otherwise  = elementAt xs (n-1)
--- TestProblems.hs
+++ TestProblems.hs
@@ -1,11 +1,15 @@
 import Test.HUnit
 import H99Solutions
 
-test1 = TestCase (assertEqual "myLast [1,2,3,4]" 4 (myLast [1,2,3,4]))
-test2 = TestCase (assertEqual "myButLast [1,2,3,4]" 3 (myButLast [1,2,3,4]))
+test1   = TestCase (assertEqual "Solution1 does not work" 4   (myLast [1,2,3,4]))
+test2   = TestCase (assertEqual "Solution2 does not work" 3   (myButLast [1,2,3,4]))
+test3_1 = TestCase (assertEqual "Solution3 does not work" 2   (elementAt [1,2,3] 2))
+test3_2 = TestCase (assertEqual "Solution3 does not work" 'e' (elementAt "haskell" 5))
 
 tests = TestList [ TestLabel "solution1" test1,
-                   TestLabel "solution2" test2]
+                   TestLabel "solution2" test2,
+                   TestLabel "solution2" test3_1,
+                   TestLabel "solution2" test3_2]
 
 main::IO Counts
 main = do
