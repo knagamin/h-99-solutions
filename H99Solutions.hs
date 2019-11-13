a44c82ef55ee66f4acfe2f6aa1eba886246f9eca
Kiyo Nagamine <knagamin.hub@gmail.com>
2019-11-13 14:07:00 +0900

    change: added resolution for Problem7

--- H99Solutions.hs
+++ H99Solutions.hs
@@ -28,3 +28,8 @@ isPalindrome [] = True
 isPalindrome [_] = True
 isPalindrome xs = (head xs) == (last xs) && (isPalindrome . tail . init) xs
 
+data NestedList a = Elem a | List [NestedList a]
+
+myFlatten :: NestedList a -> [a]
+myFlatten (Elem x) = [x]
+myFlatten (List x) = concatMap myFlatten x
